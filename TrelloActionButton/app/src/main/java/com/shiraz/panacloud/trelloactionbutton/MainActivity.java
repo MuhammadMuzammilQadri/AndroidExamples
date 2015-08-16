ed03d2ac1470ad7c84494d60ef1d777e35f99b21
ShirazShahnawaz <shiraz.shahnawaz92@gmail.com>
2015-08-15 20:50:03 -0700

    floating action button.. Done

---
+++ TrelloActionButton/app/src/main/java/com/shiraz/panacloud/trelloactionbutton/MainActivity.java
@@ -0,0 +1,168 @@
+package com.shiraz.panacloud.trelloactionbutton;
+
+import android.media.Image;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    ImageButton btn1,btn2,btn3,btn4,feb;
+    TextView tv1,tv2,tv3,tv4;
+    Animation menuAnimation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (ImageButton) findViewById(R.id.item1);
+        btn2 = (ImageButton) findViewById(R.id.item2);
+        btn3 = (ImageButton) findViewById(R.id.item3);
+        btn4 = (ImageButton) findViewById(R.id.item4);
+        feb = (ImageButton) findViewById(R.id.btnFab);
+
+        tv1 = (TextView) findViewById(R.id.text1);
+        tv2 = (TextView) findViewById(R.id.text2);
+        tv3 = (TextView) findViewById(R.id.text3);
+        tv4 = (TextView) findViewById(R.id.text4);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "btn1 click", Toast.LENGTH_SHORT).show();
+                animations();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "btn2 click", Toast.LENGTH_SHORT).show();
+                animations();
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "btn3 click", Toast.LENGTH_SHORT).show();
+                animations();
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "btn4 click", Toast.LENGTH_SHORT).show();
+                animations();
+            }
+        });
+        feb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "feb click", Toast.LENGTH_SHORT).show();
+                animations();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void animations() {
+        if (btn1.getVisibility() == View.VISIBLE) {
+            AnimationSet animationSet = new AnimationSet(false);
+            Animation slide_out = AnimationUtils.loadAnimation(this, R.anim.abc_slide_out_bottom);
+            menuAnimation = AnimationUtils.loadAnimation(this, R.anim.abc_fade_out);
+            slide_out.setDuration(500);
+            animationSet.addAnimation(menuAnimation);
+            animationSet.addAnimation(slide_out);
+
+            btn1.startAnimation(animationSet);
+            btn2.startAnimation(animationSet);
+            btn3.startAnimation(animationSet);
+            btn4.startAnimation(animationSet);
+            tv1.startAnimation(animationSet);
+            tv2.startAnimation(animationSet);
+            tv3.startAnimation(animationSet);
+            tv4.startAnimation(animationSet);
+
+            animationSet.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    btn1.setVisibility(View.GONE);
+                    btn2.setVisibility(View.GONE);
+                    btn3.setVisibility(View.GONE);
+                    btn4.setVisibility(View.GONE);
+
+                    tv1.setVisibility(View.GONE);
+                    tv2.setVisibility(View.GONE);
+                    tv3.setVisibility(View.GONE);
+                    tv4.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+        } else {
+            AnimationSet animationSet = new AnimationSet(false);
+            menuAnimation = AnimationUtils.loadAnimation(this, R.anim.fab_scale_up);
+            Animation slide_in = AnimationUtils.loadAnimation(this, R.anim.abc_slide_in_bottom);
+            slide_in.setDuration(200);
+            animationSet.addAnimation(menuAnimation);
+            animationSet.addAnimation(slide_in);
+            animationSet.addAnimation(AnimationUtils.loadAnimation(this, R.anim.abc_fade_in));
+            btn1.setVisibility(View.VISIBLE);
+            btn2.setVisibility(View.VISIBLE);
+            btn3.setVisibility(View.VISIBLE);
+            btn4.setVisibility(View.VISIBLE);
+
+            tv1.setVisibility(View.VISIBLE);
+            tv2.setVisibility(View.VISIBLE);
+            tv3.setVisibility(View.VISIBLE);
+            tv4.setVisibility(View.VISIBLE);
+
+            btn1.startAnimation(animationSet);
+            btn2.startAnimation(animationSet);
+            btn3.startAnimation(animationSet);
+            btn4.startAnimation(animationSet);
+
+            tv1.startAnimation(animationSet);
+            tv2.startAnimation(animationSet);
+            tv3.startAnimation(animationSet);
+            tv4.startAnimation(animationSet);
+        }
+    }
+}
