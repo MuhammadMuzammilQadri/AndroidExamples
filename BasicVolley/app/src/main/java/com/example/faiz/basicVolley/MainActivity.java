c17029043ce7c3241cd5c3403929406caab73013
faiz636 <muhammad_faiz@live.com>
2016-01-28 19:41:31 +0500

    custom string volley request with data

--- BasicVolley/app/src/main/java/com/example/faiz/basicVolley/MainActivity.java
+++ BasicVolley/app/src/main/java/com/example/faiz/basicVolley/MainActivity.java
@@ -13,6 +13,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
@@ -48,64 +49,55 @@ public class MainActivity extends ActionBarActivity {
 
         setPostData();
 
-//        jsonObjectRequest();
-//        stringRequest();
-        JsonArrayRequest();
+        stringRequestWithData(getString(R.string.send_data_url));
+
+        stringRequest(getString(R.string.request_url1));
+        stringRequest(getString(R.string.request_url2));
+
+        jsonObjectRequest(getString(R.string.request_url1));
+        jsonObjectRequest(getString(R.string.request_url2));
+
+        JsonArrayRequest(getString(R.string.request_url1));
+        JsonArrayRequest(getString(R.string.request_url2));
+
 
 
     }
 
-    public void setPostData(){
+    public void setPostData() {
 //        JSONObject to send post request arguments
         jsonObject = new JSONObject();
 
         try {
 //            setting data
-            jsonObject.put("CustomerName",getString(R.string.CustomerName));
-            jsonObject.put("Customerid",getString(R.string.Customerid));
+            jsonObject.put("CustomerName", getString(R.string.CustomerName));
+            jsonObject.put("Customerid", getString(R.string.Customerid));
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
     }
 
-    public void jsonObjectRequest(){
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST
-                , getString(R.string.request_url2)
-                , jsonObject
-                , new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject jsonObject) {
-                mTextView.setText(jsonObject.toString());
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                volleyError.printStackTrace();
-                Toast.makeText(getApplication(),"bad",Toast.LENGTH_SHORT).show();
-            }
-        }
-        );
+    public void stringRequest(final String url) {
 
-        queue.add(jsonObjectRequest);
-
-    }
-
-    public void stringRequest(){
-
-        StringRequest sr = new StringRequest(Request.Method.POST, getString(R.string.request_url2), new Response.Listener<String>() {
+        StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(final String response) {
+                mTextView.setText(response);
+                Log.d("response", "url-->" + url + "--value-->" + response);
                 try {
-//                    JSONObject jsonObject1 = new JSONObject(response);
                     JSONArray jsonArray = new JSONArray(response);
-                    mTextView.setText(jsonArray.getJSONObject(0).toString());
-//                    mTextView.setText(jsonObject1.toString());
+                    Log.d("responsearray", "url-->" + url + "--value-->" + jsonArray.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                mTextView.setText(response);
-                Log.d("test",response);
+
+                try {
+                    JSONObject jsonObject1 = new JSONObject(response);
+                    Log.d("responseobj", "url-->" + url + "--value-->" + jsonObject1.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
             }
         }, new Response.ErrorListener() {
@@ -133,15 +125,62 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
-//    create a json array request
-    public void JsonArrayRequest(){
+    public void stringRequestWithData(final String url) {
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put(getString(R.string.send_data_key), getString(R.string.send_data_string));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        MyStringRequest myStringRequest = new MyStringRequest(Request.Method.POST, url, jsonObject.toString(), new Response.Listener<String>() {
+            @Override
+            public void onResponse(final String response) {
+                mTextView.setText(response);
+                Log.d("response", "url-->" + url + "--value-->" + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        queue.add(myStringRequest);
+    }
+
+    public void jsonObjectRequest(final String url) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST
+                , url
+                , jsonObject
+                , new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                Log.i("response", "url-->" + url + "--value-->" + jsonObject.toString());
+                mTextView.setText(jsonObject.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                volleyError.printStackTrace();
+                Toast.makeText(getApplication(), "bad", Toast.LENGTH_SHORT).show();
+            }
+        }
+        );
+
+        queue.add(jsonObjectRequest);
+
+    }
+
+    //    create a json array request
+    public void JsonArrayRequest(String url) {
 //        create success listener
         Response.Listener<JSONArray> jsonArrayResponseListener = new Response.Listener<JSONArray>() {
 
             @Override
             public void onResponse(JSONArray jsonArray) {
                 mTextView.setText(jsonArray.toString());
-                Log.d("TEST",jsonArray.toString());
+                Log.d("TEST", jsonArray.toString());
             }
         };
 
@@ -155,9 +194,9 @@ public class MainActivity extends ActionBarActivity {
 
 //        create custom json array request
         MYJsonArrayRequest myJsonArrayRequest = new MYJsonArrayRequest(
-                getString(R.string.request_url2),
+                url,
                 jsonObject,
-                jsonArrayResponseListener ,
+                jsonArrayResponseListener,
                 errorListener
         );
 
@@ -167,11 +206,12 @@ public class MainActivity extends ActionBarActivity {
 
 }
 
+
 /*
     custom json request array implementation
     just copy past from decomplie class for JsonArrayRequest
     because default implementation is for GET request only
-* */
+*/
 class MYJsonArrayRequest extends JsonRequest<JSONArray> {
     public MYJsonArrayRequest(String url, JSONObject jsonRequest, Response.Listener<JSONArray> listener, Response.ErrorListener errorListener) {
         super(Method.POST, url, jsonRequest.toString(), listener, errorListener);
@@ -187,4 +227,33 @@ class MYJsonArrayRequest extends JsonRequest<JSONArray> {
             return Response.error(new ParseError(var4));
         }
     }
+}
+
+
+/*
+     custom string request with data string
+*/
+class MyStringRequest extends StringRequest {
+
+    private String mRequestBody;
+
+    public MyStringRequest(int method, String url, String data, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+        super(method, url, listener, errorListener);
+        mRequestBody = data;
+    }
+
+    @Override
+    public String getBodyContentType() {
+        return String.format("application/json; charset=%s", new Object[]{"utf-8"});
+    }
+
+    public byte[] getBody() {
+        try {
+            return this.mRequestBody == null?null:this.mRequestBody.getBytes("utf-8");
+        } catch (UnsupportedEncodingException var2) {
+            VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", new Object[]{this.mRequestBody, "utf-8"});
+            return null;
+        }
+    }
+
 }
