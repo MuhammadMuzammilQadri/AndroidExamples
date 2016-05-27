1cf327a8fca96be1c49fa863a4042c25ab11eddd
Umair026 <uumair99@hotmail.com>
2016-05-27 08:46:58 -0700

    error while signing in

---
+++ FirebaseAuth/app/src/main/java/com/example/umairomii/firebaseauth/AuthFragment.java
@@ -0,0 +1,148 @@
+package com.example.umairomii.firebaseauth;
+
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AuthFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AuthFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private FramentInteractionlistener listener;
+    private FirebaseAuth mAuth;
+    private DatabaseReference databaseReference;
+
+
+    public AuthFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static AuthFragment newInstance(String param1, String param2) {
+        AuthFragment fragment = new AuthFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_blank, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        final EditText email = (EditText) view.findViewById(R.id.signupEmail);
+        final EditText signInemail = (EditText) view.findViewById(R.id.signinEmail);
+        final EditText pass = (EditText) view.findViewById(R.id.signuppassword);
+        final EditText signInpass = (EditText) view.findViewById(R.id.signinpassword);
+
+        Button signUp = (Button) view.findViewById(R.id.signupButton);
+        Button signIn = (Button) view.findViewById(R.id.signinButton);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+
+                String userEmail = email.getText().toString();
+                String userPass = pass.getText().toString();
+
+                if(!userEmail.equals("") && !userPass.equals("")){
+                    Log.d("TAG1",userEmail +" "+userPass);
+                    mAuth.createUserWithEmailAndPassword(userEmail,userPass).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                            if(!task.isSuccessful()){
+                                Snackbar.make(v,"error signUp",Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+                            }else {
+                                Snackbar.make(v,"success signUp",Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+//                                Log.d("token",task.getResult().getUser().getToken(true)+"");
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                final ProgressDialog progressDialog = ProgressDialog.show(getContext(),"","wait..",false);
+                String userEmail = signInemail.getText().toString();
+                String userPass = signInpass.getText().toString();
+
+                if(!userEmail.equals("") && !userPass.equals("")) {
+                    Log.d("TAG2", userEmail + " " + userPass);
+                    mAuth.signInWithEmailAndPassword(userEmail, userPass).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            progressDialog.dismiss();
+                            if (!task.isSuccessful()) {
+                                Snackbar.make(v, "error signIn", Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+                                Log.d("exception ", task.getException().toString() + "");
+                            } else {
+                                Snackbar.make(v, "success signIn", Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        return view;
+    }
+
+    public interface FramentInteractionlistener{
+        void signIn();
+        void signUp();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+}
---
+++ FirebaseAuth/app/src/main/java/com/example/umairomii/firebaseauth/Frag1.java
@@ -0,0 +1,63 @@
+package com.example.umairomii.firebaseauth;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by umairomii on 5/26/16.
+ */
+public class Frag1 extends Fragment {
+
+    private FragmentInteraction mListener;
+
+    public Frag1(){
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view  =  inflater.inflate(R.layout.login_frag,container,false);
+
+        Button auth = (Button) view.findViewById(R.id.auth);
+        auth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            mListener.Auth();
+            }
+        });
+
+        Button cloudMsg = (Button) view.findViewById(R.id.cloudMsg);
+        cloudMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(),CloudMesgActivity.class);
+                startActivity(i);
+            }
+        });
+
+        return view;
+    }
+    interface FragmentInteraction{
+        void Auth();
+        void CloudMsg();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mListener = (FragmentInteraction) getActivity();
+    }
+}
